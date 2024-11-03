627fba1db213cd77e3255000e2b1cfb078c420aa
Typer6777 <malikzain143654@gmail.com>
2024-11-03 15:52:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showFeatures() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("features").style.display = "block";
+}
+
+function showHome() {
+    document.getElementById("features").style.display = "none";
+    document.getElementById("home").style.display = "block";
+}
+
